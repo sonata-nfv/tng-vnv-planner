05d1246649cb2a6c2e5c4ab7af756575ba5cc3b2
George Andreou <allemaos@gmail.com>
2019-04-22 20:35:55 +0100

    update the test through jenkins or just bash

--- src/test/resources/static/json/test-vnv-e2e.v2.sh
+++ src/test/resources/static/json/test-vnv-e2e.v2.sh
@@ -7,18 +7,13 @@ service_uuid=`curl -s http://pre-int-vnv-bcn.5gtango.eu:32002/api/v3/services/ |
 
 cat <<EOF > planner-input.json
 {
-  "id": 100,
   "test_plans": [
     {
       "description": "Test Plan Description: Request of an adhoc testPlan of stored TD,NSD [integrationtests at 17:37utc 2019/04/22]",
-      "id": 8,
-      "index": 3,
       "service_uuid": $service_uuid,
-      "test_uuid": $test_uuid,
-      "status": "INTEGRATION_TESTING",
+      "test_uuid": $test_uuid
     }
-  ],
-  "uuid": "45678987655"
+  ]
 }
 EOF
 
